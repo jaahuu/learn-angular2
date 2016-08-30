c4640fd74f0085bfead43a58bd8d83bba41c7101
U-ONEADR\m010633 <m010633@PLNPWM15756527.oneadr.net>
2016-08-30 12:55:07 +0200

    next 2 chapters

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -11,14 +13,7 @@ import { Component } from '@angular/core';
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name">
-      </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
     styles: [`
   .selected {
@@ -68,33 +63,29 @@ import { Component } from '@angular/core';
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
-
-
-
+`],
+  providers: [HeroService]
 })
 
-export class AppComponent {
-    public heroes = HEROES;
+
+
+
+export class AppComponent implements OnInit {
+    heroes: Hero[];
     title = "Tour of Heroes";
     selectedHero: Hero;
-    onSelect(hero: Hero) { this.selectedHero = hero; }
-}
 
-export class Hero {
-  id: number;
-  name: string;
-}
+    constructor(private heroService: HeroService) { }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+    ngOnInit(): void {
+      this.getHeroes();
+    }
+
+    getHeroes(): void {
+      this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    }
+
+    onSelect(hero: Hero) {
+      this.selectedHero = hero;
+    }
+}
